3c0357df93266a1841fff31a4482bc3042394012
Jeffrey McDonald <jffrmcdonald@gmail.com>
2021-11-17 13:16:51 -0800

    cleared red ghosts

--- scripts/RedGhost.gd
+++ scripts/RedGhost.gd
@@ -4,7 +4,7 @@ var rng = RandomNumberGenerator.new()
 var direction = Vector2(0,0)
 var path = []
 export var SPEED = 0.1
-onready var walls = get_parent().get_node("TileMap")
+onready var walls = get_parent().get_node("Navigation2D/TileMap")
 
 
 # Declare member variables here. Examples:
@@ -16,24 +16,6 @@ onready var walls = get_parent().get_node("TileMap")
 func _ready():
 	pass # Replace with function body.
 
-func _process(delta):
-	rng.randomize()
-	var r = rng.randi_range(1, 4)
-	translate(Vector2(rng,rng)*delta)
-	if r == 1:
-		direction = Vector2(0, -1)
-		rotation = deg2rad(-90)
-	elif r == 2:
-		direction = Vector2(0, 1)
-		rotation = deg2rad(90)
-	elif r == 3:
-		direction = Vector2(-1, 0)
-		rotation = deg2rad(180)
-	elif r == 4:
-		direction = Vector2(1, 0)
-		rotation = deg2rad(0)
-	var pos_to_move = walls.is_tile_vacant(position, direction)
-	position = position.linear_interpolate(pos_to_move, SPEED + delta)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
