1e6481fdd3e3888e6f83a354a9320db2a45a1c75
Ani Minassians <ani.minassians@gmail.com>
2021-11-17 10:02:51 -0800

    Update RedGhost.gd

--- scripts/RedGhost.gd
+++ scripts/RedGhost.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+var rng = RandomNumberGenerator.new()
 var direction = Vector2(0,0)
 var path = []
 export var SPEED = 0.1
@@ -16,6 +17,21 @@ func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
+	rng.randomize()
+	var r = rng.randi_range(1, 4)
+	translate(Vector2(rng,rng)*delta)
+	if r == 1:
+		direction = Vector2(0, -1)
+		rotation = deg2rad(-90)
+	elif r == 2:
+		direction = Vector2(0, 1)
+		rotation = deg2rad(90)
+	elif r == 3:
+		direction = Vector2(-1, 0)
+		rotation = deg2rad(180)
+	elif r == 4:
+		direction = Vector2(1, 0)
+		rotation = deg2rad(0)
 	var pos_to_move = walls.is_tile_vacant(position, direction)
 	position = position.linear_interpolate(pos_to_move, SPEED + delta)
 
