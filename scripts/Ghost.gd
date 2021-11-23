668665e1156a18abed3940a080e4a4f45393a7ae
Jeffrey McDonald <jffrmcdonald@gmail.com>
2021-11-22 22:16:11 -0800

    added basic move with collide

--- scripts/Ghost.gd
+++ scripts/Ghost.gd
@@ -4,8 +4,9 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
-
+export var speed = 10
+onready var tilemap = get_parent().get_node("TileMap")
+var direction = Vector2(0,speed)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
@@ -16,5 +17,11 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
+func _process(delta):
+
+	var collide = move_and_collide(direction*delta*speed)
+	if collide:
+		print(collide.position)
+		var tile = tilemap.get_cellv(tilemap.world_to_map(collide.position))
+		print("Enemy collided with:"+str(tile))
 #	pass
