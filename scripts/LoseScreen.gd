93ba44094a372795cc314143250d7be48979fa2b
Jeffrey McDonald <jffrmcdonald@gmail.com>
2021-11-27 21:35:18 -0800

    added lose screen

---
+++ scripts/LoseScreen.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+func _ready():
+	pass
+
+
+func _on_RestartButton_pressed():
+	get_tree().change_scene("res://levels/Sample Level 1.tscn")
+
+
+func _on_MainMenuButton_pressed():
+	get_tree().change_scene("res://levels/Main Menu.tscn")
