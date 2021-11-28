15ba7ab71407ca228791d7f19cf8662395f954cb
aminassians <ani.minassians@gmail.com>
2021-11-27 18:25:28 -0800

    First commit

---
+++ scripts/Player.gd
@@ -0,0 +1,52 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+export var speed = 100
+var velocity = Vector2(0,0)
+var hv = Vector2(0,0)
+onready var tilemap =  get_parent().get_node("TileMap")
+onready var player = get_parent().get_node("Player")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimatedSprite.play("movement")
+	 # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	hv = velocity/2
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2(0,-speed)
+		#$soundChomp.play()
+		rotation = deg2rad(-90)
+		
+	elif Input.is_action_pressed("ui_down"):
+		velocity = Vector2(0,speed)
+		rotation = deg2rad(90)
+		
+	elif Input.is_action_pressed("ui_left"):
+		velocity = Vector2(-speed,0)
+		rotation = deg2rad(180)
+		
+	elif Input.is_action_pressed("ui_right"):
+		velocity = Vector2(speed,0)
+		rotation = deg2rad(0)
+	
+	## warp 
+	if player.position.x >445:
+		player.position.x = 10
+		player.position.y = 277
+		
+	if player.position.x<5:
+		player.position.x = 441
+		player.position.y = 277
+	
+	move_and_collide(velocity*delta)
+	
+	tilemap.eat(player.position)
+	
+	
+#	pass
---
+++ scripts/TileMap.gd
@@ -0,0 +1,44 @@
+extends TileMap
+
+
+onready var player = get_parent().get_node("Player")
+
+const tile_type = {
+	power_nav_pellet = 14,
+	nav_pellet = 13,
+	nav_tile = 12,
+	black = 11,
+	black2 = 15,
+	pellet = 5,
+	power_pellet = 6}
+	
+func _ready():
+	pass
+
+func eat(pos):
+	var current_tile = world_to_map(pos)
+	var tile = get_cellv(current_tile)
+	if(tile == tile_type.pellet or tile == tile_type.power_pellet):
+		set_cellv(current_tile, tile_type.black2)
+		$chomp.play()
+
+func _process(delta):
+	var count = 0
+	
+	for i in range(get_used_rect().size.x):
+		
+		for j in range(get_used_rect().size.y+10):
+			
+			var tile = get_cell(i,j)
+			if(tile == tile_type.pellet or tile==tile_type.nav_pellet
+			or tile== tile_type.nav_pellet or tile==tile_type.power_nav_pellet):
+				count += 1	
+	print("Pellets eaten:"+str(242-count))
+	
+	if(count == 0):
+		#print("won")
+		$win.play()
+		get_tree().change_scene("res://assets/game assets/misc/WinScreen.tscn")
+		set_process(false)
+	#else:
+		#print(count)
