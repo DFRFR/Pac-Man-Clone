a3a644f065073ab20bdae5c972d77e9d87c60bb6
Jonathan Galon <jonjongalon@gmail.com>
2021-12-07 17:39:09 -0800

    adjusted directions and music

--- assets/game assets/misc/WinScreen.gd
+++ assets/game assets/misc/WinScreen.gd
@@ -2,8 +2,12 @@ extends CanvasLayer
 
 
 func _ready():
-	pass
+	$YouWin.play()
 
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		get_tree().change_scene("res://levels/Main Menu.tscn")
+		queue_free()
 
 func _on_RestartButton_pressed():
 	get_tree().reload("res://levels/Sample Level 1.tscn")
